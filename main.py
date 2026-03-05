6ad84262173e1ac944c8e3991607d4073d6655b3
gomesleke <voyagerleke@gmail.com>
2026-03-05 18:27:04 -0300

    add macth no main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def clear():
         os.system("clear")
 
     
-def selection():
+def selection_show():
     print("Selecione:\n")
     print("1-Começar Estudos")
     print("2-Relatório")
@@ -17,6 +17,47 @@ def selection():
     print("4-Metas")
     print("5-Configurações")
     print("6-Sair")
+
+def selection_choice():
+
+    try:
+        index_user=int(input("\nEscolha o indice: "))
+
+        match index_user:
+            
+            case 1:
+                print("qualquer coisa")
+            
+            case 2:
+                print("qualquer coisa")
+
+            
+            case 3:
+                print("qualquer coisa")
+
+            
+            case 4:
+                print("qualquer coisa")
+
+
+            case 5:
+                print("qualquer coisa")
+
+            
+            case 6:
+                print("qualquer coisa")
+            
+            case _:
+                input("Opção Inválida - Pressione qualquer Tecla")
+                main()          
+
+
+    
+    except ValueError,:
+        input("Erro: Tente Novamente - Pressione qualquer Tecla ")
+        main()
+        
+    
 
 
 def menu():
@@ -27,14 +68,13 @@ def menu():
     print("\nSTUDY TRACKER (CLI)\n")
     print("============\n")
 
-    selection()
+    selection_show()
 
 
 def main():
     menu()
+    selection_choice()
     
-
-
 
 if __name__=="__main__":
     main()
